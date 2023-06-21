199d52d1a9c4155061567cf2096e274e0717b954
Andrei Rusu <andrei.rusu@beatfactor.net>
2023-06-21 21:43:57 +0200

    updated

--- assets/minimize-32e4e56e.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./LICENSE-e4c34b63.js";import"./app-esm-27666542.js";o(()=>{setTimeout(()=>{Prism.highlightAll()},0)},{forPage:()=>!0});
---
+++ assets/mock-network-response-beb76bda.js
@@ -0,0 +1,1 @@
+import{o}from"./README-e4c34b63.js";import"./app-esm-96ad472b.js";o(()=>{setTimeout(()=>{Prism.highlightAll()},0)},{forPage:()=>!0});
