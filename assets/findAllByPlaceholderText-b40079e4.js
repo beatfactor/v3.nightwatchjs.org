0f7ddb651af1f1a706db07c19407eb5c9ef657df
Andrei Rusu <andrei.rusu@beatfactor.net>
2023-06-22 16:43:10 +0200

    updated

---
+++ assets/findAllByPlaceholderText-b40079e4.js
@@ -0,0 +1,1 @@
+import{o}from"./index-8fb8d7bd.js";import"./app-esm-288bbb06.js";o(()=>{setTimeout(()=>{Prism.highlightAll()},0)},{forPage:()=>!0});
--- assets/maximize-beb76bda.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./README-e4c34b63.js";import"./app-esm-96ad472b.js";o(()=>{setTimeout(()=>{Prism.highlightAll()},0)},{forPage:()=>!0});
